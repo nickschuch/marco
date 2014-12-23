2d5b6db53a0b501a4a94298b5602fc04bb63684b
Nick Schuch <nick@myschuch.com>
2014-12-23 10:42:07 +0000

    Move main.go -> marco.go

--- marco.go
+++ marco.go
@@ -16,20 +16,36 @@ var ports string
 func handler(w http.ResponseWriter, r *http.Request) {
     // Get the name of the container based on the domain sent to this proxy.
     // eg. project1.example.com = project1
+    //
+    // Todo:
+    //   * What happens when we don't have a subdomain provided.
     s := strings.Split(r.Host, ".")
     name := s[0]
 
+    // Connect to the Docker daemon with the flag.
     docker, _ := dockerclient.NewDockerClient(endpoint, nil)
     container, err := docker.InspectContainer(name)
     if err != nil {
         log.Fatal(err)
     }
 
+    // Query Docker for the IP of the container.
+    //
+    // Todo:
+    //   * What happens when we don't have an IP.
     ip := container.NetworkSettings.IpAddress
     if err != nil {
         log.Fatal(err)
     }
 
+    // The first port available via the "ports" argument.
+    // eg. When using the default ports flag value.
+    //     If the port exposes 8080 and 2368 than it will
+    //     be 8080.
+    //
+    // Todo:
+    //   * What happens when we don't have a port?
+    //   * Logging.
     port := ""
     for exposed := range container.NetworkSettings.Ports {
         p := strings.Split(exposed, "/")
@@ -39,11 +55,15 @@ func handler(w http.ResponseWriter, r *http.Request) {
         }
     }
 
+    // Proxy through to the container.
+    //
+    // Todo:
+    //   * Cache the final proxy so we don't have to compute it on every request.
+    //   * Logging.
     remote, err := url.Parse("http://" + ip + ":" + port)
     if err != nil {
        panic(err)
     } 
-
     proxy := httputil.NewSingleHostReverseProxy(remote)
     proxy.ServeHTTP(w, r)
 }
