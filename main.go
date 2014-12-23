9bf518794a3e0fb26688f81d09dee27671419d31
Nick Schuch <nick@myschuch.com>
2014-12-23 10:13:55 +0000

    Able to query Docker.

--- main.go
+++ main.go
@@ -1,21 +1,47 @@
 package main
 
 import (
-    "math/rand"
+    "flag"
+    "log"
+    "strings"
     "net/url"
     "net/http"
     "net/http/httputil"
+    "github.com/samalba/dockerclient"
 )
 
+var endpoint string
+var ports string
+
 func handler(w http.ResponseWriter, r *http.Request) {
-    answers := []string{
-      "http://www.google.com",
-      "http://172.17.0.2:2368",
+    // Get the name of the container based on the domain sent to this proxy.
+    // eg. project1.example.com = project1
+    s := strings.Split(r.Host, ".")
+    name := s[0]
+
+    docker, _ := dockerclient.NewDockerClient(endpoint, nil)
+    container, err := docker.InspectContainer(name)
+    if err != nil {
+        log.Fatal(err)
     }
 
-    remote, err := url.Parse(answers[rand.Intn(len(answers))])
+    ip := container.NetworkSettings.IpAddress
     if err != nil {
-      panic(err)
+        log.Fatal(err)
+    }
+
+    port := ""
+    for exposed := range container.NetworkSettings.Ports {
+        p := strings.Split(exposed, "/")
+        if strings.Contains(ports, p[0]) {
+            port = p[0]
+            break
+        }
+    }
+
+    remote, err := url.Parse("http://" + ip + ":" + port)
+    if err != nil {
+       panic(err)
     } 
 
     proxy := httputil.NewSingleHostReverseProxy(remote)
@@ -23,7 +49,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+    flag.StringVar(&endpoint, "endpoint", "unix:///var/run/docker.sock", "The Docker API endpoint eg. tcp://localhost:2375")
+    flag.StringVar(&ports, "ports", "80,8080,2368,8983", "The ports you wish to proxy. Ordered in preference eg. 80,2368,8983")
+    flag.Parse()
+
     http.HandleFunc("/", handler)
+    // @todo, Make this an option.
     http.ListenAndServe(":80", nil)
 }
-
